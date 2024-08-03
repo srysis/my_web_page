5182502d8e115ff4388ce2850fa3665a3e77c9a1
srysis <jashandenis@gmail.com>
2024-08-03 18:09:52 +0200

    created a script that handles links to corresponding pages

---
+++ portfolio/characters/scripts/main.js
@@ -0,0 +1,15 @@
+document.querySelector("div.link_to_character.alice").addEventListener('click', function() {
+	document.location.href = "alice.html";
+});
+
+document.querySelector("div.link_to_character.jane").addEventListener('click', function() {
+	document.location.href = "jane.html";
+});
+
+document.querySelector("div.link_to_character.pandemonica").addEventListener('click', function() {
+	document.location.href = "pandemonica.html";
+});
+
+document.querySelector("div.link_to_character.willow").addEventListener('click', function() {
+	document.location.href = "willow.html";
+});
