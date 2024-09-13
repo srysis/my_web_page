a56c76016c98b94aa9ab97fb9f9a8186142452fe
srysis <jashandenis@gmail.com>
2024-09-13 22:55:52 +0200

    added functionality for displaying a small info box with a link that leads to a landing page template website

--- portfolio/site_3_react_port/src/AppRoot.jsx
+++ portfolio/site_3_react_port/src/AppRoot.jsx
@@ -7,8 +7,18 @@ import Footer from "./components/Footer"
 
 import AsideTogglerButton from "./components/AsideTogglerButton"
 
+import cross_icon from "./files/cross.svg"
+
 function AppRoot() {
 	const [visible, setVisibility] = React.useState(false);
+	const [showInfo, setShowInfo] = React.useState(false);
+
+	React.useEffect(() => {
+		const timer = setTimeout(() => {
+			setShowInfo(true);
+		}, 1000);
+		return () => clearTimeout(timer);
+	}, []) 
 
 	function setAsideVisibility() {
 		setVisibility(!visible);
@@ -16,6 +26,12 @@ function AppRoot() {
 
 	return (
 		<>
+			{ showInfo &&  
+				<div id="info">
+					<p>This website is created based on the template, that is available by using <a href="https://html.design/download/donila-wedding-psd-template/" >this</a> link.</p>
+					<img src={cross_icon} onClick={() => setShowInfo(false)} />
+				</div>
+			}
 			<AsideTogglerButton toggleVisibilityFunction={setAsideVisibility} />
 			<Header />
 			<Aside toggleVisibilityFunction={setAsideVisibility} visibility={visible} />
