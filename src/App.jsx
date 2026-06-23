27103fef2feb9ad678a5e209fccaf71e843a45ff
srysis <jashandenis@gmail.com>
2026-06-23 13:39:30 +0200

    code refactor: no longer utilizes 'routing', renders only the 'Home' page; removed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Routes, Route } from 'react-router-dom'
 
 import Home from "./pages/home"
-import Portfolio from "./pages/portfolio"
-import NotFoundPage from "./pages/not_found_page.jsx"
 
 import "./style/style.css"
 import "./style/style-phone.css"
 
 function App() {
-	window.history.scrollRestoration = 'manual';
-
 	return (
-		<HashRouter basename="/">
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/portfolio" element={<Portfolio />} />
-				<Route path="*" element={<NotFoundPage />} />
-			</Routes>
-		</HashRouter>
+		<Home />
 	);
 }
 
