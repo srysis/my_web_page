da41b51b549c97f2d0031d9403b229c947ff4e54
srysis <jashandenis@gmail.com>
2026-06-23 13:46:51 +0200

    content and structure overhaul: now only showcases 'full' projects, changed the navigation

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -1,60 +1,70 @@
-import { useEffect } from 'react'
-import { Link } from "react-router-dom"
+import game_shop_image from "../files/portfolio/game_shop.png"
+import forum_image from "../files/portfolio/forum.png"
+import cookbook_image from "../files/portfolio/cookbook.png"
 
-function PortfolioSection( {all_animations_played} ) {
-	// function that checks if given element is in the user's browser's viewport by X percent, where X is from 0 to 100
-	function isVisibleInViewport(element, percentage) {
-		let rect = element.getBoundingClientRect();
-		let windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-
-		return !(
-			Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-rect.height) * 100)) < percentage ||
-			Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentage
-		)
-	};
-
-
-	function onDekstopScroll() {
-		if (isVisibleInViewport(document.querySelector("section#portfolio"), 3)) {
-			document.querySelector("section#portfolio").classList.add("animate");
-			document.querySelector("section#portfolio").classList.add("left_to_right");
-		}
-	}
-
-	function onMobileScroll() {
-		if (isVisibleInViewport(document.querySelector("section#portfolio"), 1)) {
-			document.querySelector("section#portfolio").classList.add("animate");
-			document.querySelector("section#portfolio").classList.add("fade_in");
-		}
-	}
-
-	useEffect(() => {
-		if (!all_animations_played) {
-			if (window.innerWidth > 550) { // check if it's not a mobile device
-				window.addEventListener("scroll", onDekstopScroll);
-			} else if (window.innerWidth < 550) { // works if it is a mobile device
-				window.addEventListener("scroll", onMobileScroll);
-			}
-		} else {
-			window.removeEventListener("scroll", onDekstopScroll);
-			window.removeEventListener("scroll", onMobileScroll);
-		}
-
-		return () => { window.removeEventListener("scroll", onDekstopScroll); window.removeEventListener("scroll", onMobileScroll); }
-	}, [all_animations_played]);
-
-
-	function onClickHandler() {
-		window.sessionStorage.setItem('scroll_pos', JSON.stringify(window.scrollY));
-	}
-
+function PortfolioSection( ) {
 	return (
-		<section id="portfolio" className={all_animations_played ? "animated" : ""}>
-			<h2>My portfolio</h2>
+		<section id="portfolio">
+			<h2>Portfolio</h2>
 			<hr />
-			<div className="text">
-				<p>I have been working on several small projects, but I have also worked on replication of pre-existing templates, that are freely available.</p>
-				<p style={{ "marginTop" : "10px" }}><strong><Link to="/portfolio"  onClick={onClickHandler}>You can access it here.</Link></strong></p>
+			<div id="list">
+				<div className="project">
+					<div className="overlay">
+						<a href="https://srysis-game-shop.netlify.app" target="_blank" rel="noreferrer"></a>
+					</div>
+					<div className="preview_image">
+						<img src={game_shop_image} title="Game Shop" alt="Game Shop" />
+					</div>
+					<div className="info">
+						<h3>Game Shop</h3>
+						<p>A 'proof-of-concept' online shop.</p>
+						<div>
+							<span>React.js</span>
+							<span>Node.js</span>
+							<span>Express</span>
+							<span>MySQL</span>
+						</div>
+					</div>
+				</div>
+				<div className="project">
+					<div className="overlay">
+						<a href="https://srysis-forum.netlify.app" target="_blank" rel="noreferrer"></a>
+					</div>
+					<div className="preview_image">
+						<img src={forum_image} title="Forum" alt="Forum" />
+					</div>
+					<div className="info">
+						<h3>Forum</h3>
+						<p>A full-stack forum web-application. Discuss anything or share your pictures!</p>
+						<div>
+							<span>React.js</span>
+							<span>Node.js</span>
+							<span>Express</span>
+							<span>JWT</span>
+							<span>MySQL</span>
+						</div>
+					</div>
+				</div>
+				<div className="project">
+					<div className="overlay">
+						<a href="https://srysis-cookbook.netlify.app" target="_blank" rel="noreferrer"></a>
+					</div>
+					<div className="preview_image">
+						<img src={cookbook_image} title="Cookbook" alt="Cookbook" />
+					</div>
+					<div className="info">
+						<h3>Cookbook</h3>
+						<p>A personal 'recipe book' website. Create your recipes and search through them to find a specific recipe.</p>
+						<div>
+							<span>React.js</span>
+							<span>i18n</span>
+							<span>Node.js</span>
+							<span>Express</span>
+							<span>JWT</span>
+							<span>MySQL</span>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	)
