3c56bd12f4a3d55df8dc5d5b1654e08425f62b35
srysis <jashandenis@gmail.com>
2026-06-23 13:45:21 +0200

    create new 'MinorSkills' component that will showcase skills that I am not as proficient in as in major skills

---
+++ src/components/MinorSkills.jsx
@@ -0,0 +1,18 @@
+
+function MinorSkills( ) {
+	return (
+		<div id="minor_skills">
+			<h2>Minor skills</h2>
+			<hr />
+            <ul>
+				<li>Tailwind CSS</li>
+				<li>Adobe PhotoShop/Paint.net</li>
+				<li>Canva (for website making)</li>
+                <li>Apache 2.4</li>
+				<li>Blender</li>
+			</ul>
+		</div>
+	)
+}
+
+export default MinorSkills;
