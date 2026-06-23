9b6f630f5c2f1a6c1e752bb6f33958d3970a4c14
srysis <jashandenis@gmail.com>
2026-06-23 13:41:42 +0200

    renamed 'Header' into 'Intro', changed the contents of the file, changed 'id' attribute of 'section'

---
+++ src/components/Intro.jsx
@@ -0,0 +1,18 @@
+
+function Intro( ) {
+
+	return (
+		<section id="intro">
+			<div>
+				<h1>Denys '<em>srysis</em>' Yarymovych</h1>
+				<hr />
+				<p>
+					Web-developer with roughly 2 years of self-taught experience and almost 1 year of commercial experience. 
+					My main focus is to provide clean, simplistic and smooth UI for any web-page/web-application.
+				</p>
+			</div>
+		</section>
+	) 
+}
+
+export default Intro
