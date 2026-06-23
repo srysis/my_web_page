165a50e8abf17ed91db9ee38e859b5997023db94
srysis <jashandenis@gmail.com>
2026-06-23 13:43:38 +0200

    renamed 'Skills' into 'MajorSkills'; structure and content overhaul

---
+++ src/components/MajorSkills.jsx
@@ -0,0 +1,21 @@
+
+
+function MajorSkills( ) {
+	return (
+		<div id="major_skills">
+			<h2>Major skills</h2>
+			<hr />
+			<ul>
+				<li>HTML, CSS</li>
+				<li>JavaScript/TypeScript</li>
+				<li>React.js</li>
+				<li>Node.js, Express, JWT</li>
+				<li>MySQL</li>
+				<li>Postman</li>
+				<li>GitHub</li>
+			</ul>
+		</div>
+	)
+}
+
+export default MajorSkills;
