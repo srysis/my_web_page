9ff2be31d158e981684b75de981c3e4f3758b31f
srysis <jashandenis@gmail.com>
2026-06-24 13:33:14 +0200

    import and use hooks, utilize 'useEffect' to set a default language on start

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useTranslation } from "react-i18next";
 
 import Home from "./pages/home"
 
@@ -6,6 +7,14 @@ import "./style/style.css"
 import "./style/style-phone.css"
 
 function App() {
+	const { i18n } = useTranslation();
+
+	useEffect(() => {
+		if (!window.localStorage.getItem("lang")) {
+			window.localStorage.setItem("lang", i18n.language);
+		}
+	}, [])
+
 	return (
 		<Home />
 	);
