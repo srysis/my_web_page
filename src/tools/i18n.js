a82bd69b158f0ef7c2f49554228f35a9cc8f327e
srysis <jashandenis@gmail.com>
2026-06-27 13:33:09 +0200

    define 'locale_load_path' based on current environment to set 'loadPath' option for 'i18n' instance

--- src/tools/i18n.js
+++ src/tools/i18n.js
@@ -8,6 +8,14 @@ export const supportedLngs = {
   ua: "Українська"
 };
 
+let locale_load_path = '';
+
+if (process.env.NODE_ENV === "development") {
+  locale_load_path = '/locales/{{lng}}/translation.json'
+} else if (process.env.NODE_ENV === "production") {
+  locale_load_path = '/my_web_page/locales/{{lng}}/translation.json'
+}
+
 i18n
   .use(i18n_http_backend)
   .use(initReactI18next)
@@ -18,6 +26,9 @@ i18n
     interpolation: {
       escapeValue: false
     },
+    backend: {
+      loadPath: locale_load_path
+    }
   });
 
   export default i18n;
